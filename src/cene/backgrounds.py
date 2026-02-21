36a9e44e48ba13f39573b2473e0e6e06a929b8a6
Aztromyy <galxy.sup@gmail.com>
2026-02-21 13:21:41 -0300

    Moved to utilities

--- src/cene/backgrounds.py
+++
@@ -1,7 +0,0 @@
-import pygame 
-from root.settings import WIDTH, HEIGHT
-
-class Background:
-    def __init__(self, image):
-        self.image = pygame.image.load(image).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (WIDTH, HEIGHT))
