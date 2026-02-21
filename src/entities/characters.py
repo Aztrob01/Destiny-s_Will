96b8e069a8e31a946a80259c5ff4c8d67324c683
Aztromyy <galxy.sup@gmail.com>
2026-02-21 13:24:30 -0300

    Update characters.py

--- src/entities/characters.py
+++ src/entities/characters.py
@@ -1,10 +1,10 @@
 import pygame
 
-from root.utilities import update_sprt
+from src.root.utils import update_sprt
 
 from combat.actions import ActionSystem
-from combat.context import ContextMenu
 from combat.profile import CombatProfile
+from combat.combat_history import CombatHistory
 
 # Create the real entity thats read in game
 
@@ -12,7 +12,7 @@ class Characters:
     def __init__(self, member):
         self.origin    = member
         self.codename  = member.codename
-        self.on_screen = member.size 
+        self.on_screen = member.size
         self.chid      = 0
         
         self.main_state     = 'fighting'
@@ -22,13 +22,11 @@ class Characters:
         self.skills         = member.basic_skills
         self.passive        = member.passive
         
-        self.CONTEXT        = ContextMenu()
         self.COMBAT_PROFILE = CombatProfile(self)
-        self.COMBAT_ACTIONS = ActionSystem(self)
-        
-        self.current_lv    = 1
-        self.current_xp    = 0
-        self.total_xp      = self.current_xp
+        self.history        = CombatHistory(self)
+        self.current_lv     = 1
+        self.current_xp     = 0
+        self.total_xp       = self.current_xp
 
     def update(self):
         if self.COMBAT_PROFILE.CURRENT_HP <= 0:
