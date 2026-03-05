5853eb5be22fae1520649548c3a0c294d61fe5fa
Aztromyy <galxy.sup@gmail.com>
2026-03-05 19:26:16 -0300

    Update entities.py

--- src/entities/entities.py
+++ src/entities/entities.py
@@ -1,17 +1,20 @@
 import pygame, random
 
 from combat.combat_buffer import CombatBuffer
-from src.root.utils import update_sprt
+from root.utils import update_sprt
 
 class Entities:
     def __init__(self, entity):
         self.origin     = entity
         self.codename   = entity.codename
         self.on_screen  = entity.size
+        self.image = entity.inner_galery['fighting']['idle']
+        self.rect  = self.image.get_rect()
+
         self.randomized = False
         self.mid        = 0
         self.mlvl       = 1
-        self.buffer     = CombatBuffer()
+        self.buffer     = None
         self.main_state     = 'fighting'
         self.fighting_state = 'idle'
    
