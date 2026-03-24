55e5e4fc1da7773991c4dfc0bf84f14679f62cd6
Aztromyy <galxy.sup@gmail.com>
2026-03-24 16:04:07 -0300

    Moved into /src/game

--- src/core/combat/context.py
+++
@@ -1,8 +0,0 @@
-class ContextMenu:
-    def __init__(self):
-        self.ALLIES = []
-        
-        self.get_hitted = False
-        self.agressor   = None
-        
-        self.passive_times_used = 0
