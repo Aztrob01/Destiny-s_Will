11d4014b6edc4b4dfbfdbdbd753c7bacb0503126
Aztromyy <galxy.sup@gmail.com>
2026-03-30 14:25:45 -0300

    Refactored for better context

--- src/game/unities/player_core.py
+++
@@ -1,8 +0,0 @@
-from entities.characters import Characters
-from entities.classes import *
-
-class PlayerCore:
-    def __init__(self):
-        self.chars = [Characters(cvl()), Characters(dwf()), Characters(wzr())]
-        self.wchar = self.chars[0]
-        
