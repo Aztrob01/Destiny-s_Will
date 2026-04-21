7b20792ea2231df537c9ed04bd7092ee2c160f28
Aztromyy <galxy.sup@gmail.com>
2026-04-20 21:34:01 -0300

    Delete manager_skills.py

--- src/core/manager_skills.py
+++
@@ -1,9 +0,0 @@
-class SkillEngine:
-    def __init__(self, name, type):
-        self.name = None
-        self.type = None
-
-class DoppelPassive:
-    def __init__(self):
-        self.data = { 'family': 'Indominus', 'name': 0 }
-        
