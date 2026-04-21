07ee2b739f547d638d82b2cfebeeadb1debdb9fa
Aztromyy <galxy.sup@gmail.com>
2026-04-20 21:34:18 -0300

    Name changed to main_profile.py

--- src/core/combat/main_profile.py
+++ src/core/combat/main_profile.py
@@ -54,40 +54,4 @@ class ProfileData:
                 '4rd_sk_times_used': 0,
             }
         }
-        
-class CombatProfileData:
-    def __init__(self, char):
-        self.user      = char
-        self.user_data = char.job.data
-        self.stats     = char.profile.stats
-        self.history   = char.profile.history
-
-        for entries in self.history['received']:
-            self.history['received'][entries] = 0
-        for entries in self.history['dealt']:
-            self.history['dealt'][entries] = 0
-
-        self.history['rounds'] = {
-            'actual_round': 0, 'rounds_played': 0,
-        }
-        self.history['interactions'] = {
-            'last_target': None, 'last_agressor': 0, 'last_assistance': None, 'last_assisted': None },
     
-    def reset(self):
-        self.stats = self.user.profile.stats
-        self.history = self.user.profile.history
-        # reseted entries
-        for entries in self.history['received']:
-            self.history['received'][entries] = 0
-        for entries in self.history['dealt']:
-            self.history['dealt'][entries] = 0
-        # extra entries
-        self.history['rounds'] = {
-            'actual_round': 0, 'rounds_played': 0,
-        }
-        self.history['interactions'] = {
-            'last_target': None, 'last_agressor': 0, 'last_assistance': None, 'last_assisted': None },
-
-
-
-                        
